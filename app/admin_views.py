d7d5e6cfffc52f1cb81109783e5761516ed49717
Phenphet <potpt1098@gmail.com>
2023-11-24 13:14:36 +0700

    change a admin

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,6 +1,13 @@
 from app import app
 from flask import render_template
+from app.models import Models
 
 @app.route('/admin/')
 def admin():
+    database = Models()
+    query = database.Select_Data()
+    return render_template('admin/admin.html', datas = query['data'])
+
+@app.route('/admin/login/')
+def login():
     return render_template('admin/login.html')
