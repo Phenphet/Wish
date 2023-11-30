1794800cc454347bcc9eff27abb24f3cd7be83c8
Phenphet <potpt1098@gmail.com>
2023-11-30 22:31:32 +0700

    update admin_views.py : add , methods=[GET,POST] to func

--- app/admin_views.py
+++ app/admin_views.py
@@ -2,14 +2,15 @@ from app import app
 from flask import render_template
 from app.models import Models
 
-@app.route('/admin/table/')
+@app.route('/admin/table/', methods=['GET','POST'])
 def admin_table():
     database = Models()
     query = database.Select_Data()
     return render_template('admin/admin.html', datas = query['data'])
 
-@app.route('/admin/login/')
+@app.route('/admin/login/', methods=['GET','POST'])
 def admin_login():
+
     return render_template('admin/login.html')
 
 @app.errorhandler(404)
